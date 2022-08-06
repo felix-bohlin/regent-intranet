b9504cec89df755ba2b0b383c0dfd578ef3b5e8e
Felix <felix.bohlin@protonmail.com>
2022-08-06 09:03:26 +0200

    refactor: move files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['./.vitepress/**/*.vue'],
   theme: {
+    colors: {
+      red: '#98040c',
+      blue: '#3ec7f3',
+      lightGray: '#deddef',
+      darkGray: '#282934',
+      white: '#fffff',
+      black: '#fffff'
+    },
     extend: {
       screens: {
         lg: '960px'
